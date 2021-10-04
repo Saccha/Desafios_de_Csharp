dbc13a5e15e8dbc09f5c9b5f2b113ad662c86842
Sabrina Haßlocher <62216740+Saccha@users.noreply.github.com>
2021-10-04 18:41:05 -0300

    Create solucao.cs

---
+++ Desafios/Desafio26/solucao.cs
@@ -0,0 +1,26 @@
+using System; 
+
+class TESTE 
+{
+
+    static void Main(string[] args)
+    { 
+
+    int n=int.Parse(Console.ReadLine());
+if (  n == 1  )
+   Console.WriteLine("Top 1");
+else if (   n <= 3      )
+   Console.WriteLine("Top 3");
+else if (   n <= 5          )
+    Console.WriteLine("Top 5");
+else if (    n <= 10             )
+    Console.WriteLine("Top 10");
+else if (     n <= 25             )
+    Console.WriteLine("Top 25");
+else if (      n <= 50              )
+    Console.WriteLine("Top 50");
+else if (      n <= 100                   )
+    Console.WriteLine("Top 100"); 
+    }
+
+}
